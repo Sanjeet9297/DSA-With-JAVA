af8bba0d91943b42762a1f58baf0cd2a8590491f
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2024-05-24 18:43:48 +0530

    add new

--- Day-1/javaBasic.java
+++ Day-1/javaBasic.java
@@ -1,10 +1,8 @@
 public  class javaBasic {
 
   public static void main(String args[]) {
-      
-
-
-    
+    System.out.print("Hello Sanjeet!\n"); // 1. tarika line ka space 
+    System.out.print("Hello Sanjeet!\n"); // 2. tarika line ka space
   }
 }
 
