bd5281587fe90e5c04869a134590e0b85d387bd5
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2024-05-25 13:24:50 +0530

    add more changes

---
+++ Day-2/input.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class input{
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      // String input = sc.next();   ONLY ONE LINE PRINT AS A INPUT (sc.next) ++++++STRING++++++
+      // System.err.print(input);
+
+      // String input = sc.nextLine();   ALL LINE PRINT AS A INPUT (sc.next) ++++++STRING++++++
+      // System.err.print(input);
+
+      // int Number = sc.nextInt(); // (sc.nextInt) +++++++ INT ++++++++
+      //System.err.print(Number);
+
+      //boolean bl = sc.nextBoolean();
+      //System.err.print(bl);
+
+      // short sh = sc.nextShort();
+      //System.err.print(sh);
+
+      float fl = sc.nextFloat();
+      System.err.print(fl);
+  }
+}
