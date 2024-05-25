41dd559a230a075279e64ac9616f2eb43ccdad12
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2024-05-25 12:15:20 +0530

    add more

---
+++ Day-2/sumTwoNo.java
@@ -0,0 +1,8 @@
+public  class sumTwoNo{
+  public static void main(String[] args) {
+      int a = 20;
+      int b = 15;
+      int sum = a + b;
+      System.err.print(sum);
+  }
+}
---
+++ Day-2/variables.java
@@ -0,0 +1,10 @@
+public  class variables{
+  public static void main(String[] args) {
+      int a = 10;
+      int b = 20;
+      String name = "Sanjeet Kumar";
+      System.out.println(a);
+      System.out.println(b);
+      System.err.println(name);
+  }
+}
