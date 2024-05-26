bee5a0421969eeb348a749274e68ffe940ee3296
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2024-05-26 10:58:05 +0530

    add more

---
+++ Day-3/arithem.java
@@ -0,0 +1,12 @@
+public  class arithem{
+  public static void main(String[] args) {
+      int A = 10;
+      int B = 5;
+      System.err.println("Add = "+ (A+B));
+      System.err.println("Sub = "+ (A-B));
+      System.err.println("Mul = "+ (A*B));
+      System.err.println("Div = "+ (A/B));
+      System.err.println("Mod = "+ (A%B));
+      
+  }
+}
